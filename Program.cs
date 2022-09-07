18cba518ff5ec0429cc46d9ba429924c3a07453b
Yogi <qq643374843@163.com>
2022-09-07 21:18:09 +0800

    Firstday

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Study01;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        Console.WriteLine($"Pls input your name & birthday:\t");
+        string nameAndBirthday=Console.ReadLine().ToString();
+        Console.WriteLine("Your Name and Birthday:\t{0}",nameAndBirthday);
+        Console.WriteLine("Pls press Enter to exit…………");
+        while(Console.ReadKey().Key!=ConsoleKey.Enter){}
+    }
+}
