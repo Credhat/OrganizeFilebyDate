cc0887f8beb6e697ef9bb445c467255b57b14a13
Yooogi <qq643374843@163.com>
2022-09-08 23:22:25 +0800

    NewCodeAt8-9-2022

---
+++ OrganizedFiles.cs
@@ -0,0 +1,151 @@
+
+
+using System;
+using System.Diagnostics;
+
+
+
+public class OrganizeFilesOperation
+{
+    #region privateField
+
+    //获取软件当前位置
+    private static string defaultPath = (Directory.GetCurrentDirectory() == null) ? "Null" : Directory.GetCurrentDirectory();
+    private string userInputPath = string.Empty;
+    //获取当前目录所有文件流数组
+    private FileInfo[] fileInfo;
+    private int filesNumberTotal = 0;
+    private DirectoryInfo directoryInfo;
+
+    #endregion
+
+    // public FileInfo[] FileInfos
+    // {
+    //     get
+    //     {
+    //         return this.fileInfo;
+    //     }
+    //     set
+    //     {
+    //         this.fileInfo = value;
+    //     }
+    // }
+
+    #region  初始化构造器
+
+    public OrganizeFilesOperation()
+    {
+        this.directoryInfo = new DirectoryInfo(defaultPath.Trim('\"'));
+        this.fileInfo = directoryInfo.GetFiles();
+        this.filesNumberTotal = this.fileInfo.Count<FileInfo>();
+#if !DEBUG
+        Console.WriteLine("Created by NoneParameter Method");
+#endif
+    }
+
+    public OrganizeFilesOperation(string path)
+    {
+        //处理输入文本
+        this.userInputPath = String.IsNullOrEmpty(path) ? defaultPath : path;
+        this.userInputPath = this.userInputPath.Trim('"');
+        //处理结束
+        //判断输入字符串是文件还是文件夹
+        if (File.Exists(this.userInputPath))
+        {
+            this.fileInfo = new FileInfo[1];
+            fileInfo[0] = new FileInfo(userInputPath);
+            this.filesNumberTotal = this.fileInfo.Count<FileInfo>();
+        }
+        else if (Directory.Exists(this.userInputPath))
+        {
+            this.directoryInfo = new DirectoryInfo(this.userInputPath);
+            this.fileInfo = directoryInfo.GetFiles();
+            this.filesNumberTotal = this.fileInfo.Count<FileInfo>();
+        }
+        else
+        {
+            Console.WriteLine("---------------无效路径-------------\r\n ");
+        }
+
+#if !DEBUG
+        Console.WriteLine("Created by Parameter Method --- Path: {0}", userInputPath);
+#endif
+
+    }
+
+
+    #endregion
+
+
+    #region 文件操作函数
+    //获取所有文件数
+    public int FilesNumberTotal()
+    {
+        return filesNumberTotal;
+    }
+
+    //获取所有文件名
+    public string[] FilesName()
+    {
+        string[] tempFilesNameArray = new string[filesNumberTotal];
+        for (int i = 0; i < filesNumberTotal; i++)
+        {
+            tempFilesNameArray[i] = fileInfo[i].Name;
+        }
+        return tempFilesNameArray;
+    }
+
+    //获取所有文件创建时间(HashSet)
+    public string[] FilesCreatedTime()
+    {
+        string[] tempFilesCreatedTimeArray = new string[filesNumberTotal];
+        for (int i = 0; i < filesNumberTotal; i++)
+        {
+            DateTime fileInfoDateTime = fileInfo[i].LastWriteTime;
+            tempFilesCreatedTimeArray[i] = "Month " + fileInfoDateTime.Month + "-Day " + fileInfoDateTime.Day + "-Hr " + fileInfoDateTime.Hour + "-Y " + fileInfoDateTime.Year;
+            Console.WriteLine("Add item: {0}", tempFilesCreatedTimeArray[i]);
+        }
+        HashSet<string> createdTime_HashSet = tempFilesCreatedTimeArray.ToHashSet<string>();
+        tempFilesCreatedTimeArray = new string[createdTime_HashSet.Count];
+
+        // for (int i = 0; i < createdTime_HashSet.Count; i++)
+        // {
+
+        //     tempFilesCreatedTimeArray[i] = createdTime_HashSet[i];
+        // }
+        createdTime_HashSet.CopyTo(tempFilesCreatedTimeArray);
+
+        return tempFilesCreatedTimeArray;
+    }
+
+    //根据文件创建时间创建文件夹
+    public void CreatDirectories(string[] filesTime)
+    {
+        if (filesTime.Length <= 0 || String.IsNullOrEmpty(filesTime[0]))
+        {
+            throw new ArgumentOutOfRangeException("No Files Created Time Get!");
+        }
+
+        foreach (var item in filesTime)
+        {
+            directoryInfo.CreateSubdirectory(item);
+            Console.WriteLine("Dir: {0} created!", item);
+        }
+        Console.WriteLine("All Dir created!");
+    }
+    //获取文件InfoVersion
+    public void PrintFileInfoVersion()
+    {
+        if (filesNumberTotal > 0 && fileInfo[0].Exists)
+        {
+            System.Diagnostics.FileVersionInfo fVersionInfo = System.Diagnostics.FileVersionInfo.GetVersionInfo(fileInfo[0].FullName);
+            Console.WriteLine("\r\nFile {0} detected.\r\n-ProductVersion: {1} \r\n-FileVersion: {2}\r\n", fileInfo[0].Name, fVersionInfo.ProductVersion, fVersionInfo.FileVersion);
+        }
+        else
+        {
+            new ArgumentOutOfRangeException("文件未找到！文件位置或者文件名错误~");
+        }
+    }
+
+    #endregion
+}
--- Program.cs
+++ Program.cs
@@ -1,13 +1,88 @@
-﻿namespace Study01;
+﻿
+namespace OrganizePLNfiles;
 class Program
 {
     static void Main(string[] args)
     {
+        
+        Console.SetWindowSize(width:60,height:30);
         Console.WriteLine("Hello, World!");
-        Console.WriteLine($"Pls input your name & birthday:\t");
-        string nameAndBirthday=Console.ReadLine().ToString();
-        Console.WriteLine("Your Name and Birthday:\t{0}",nameAndBirthday);
-        Console.WriteLine("Pls press Enter to exit…………");
-        while(Console.ReadKey().Key!=ConsoleKey.Enter){}
+
+        while (true)
+        {
+            PrintFounciton();
+            string userChoice = Console.ReadLine();
+            userChoice = String.IsNullOrEmpty(userChoice) ? "Ur input is not right!Pls input right number." : userChoice;
+            
+            switch (userChoice)
+            {
+                case "1":
+                    CreatDirs();
+                    break;
+                case "2":
+                    OutputFileVersionInfo();
+                    break;
+                case "3":
+                    Console.Clear();
+                    continue;
+                case "exit":
+                    Environment.Exit(0);
+                    break;
+                default:
+                    Console.Clear();
+                    Console.WriteLine("No fucntion selected! Go back to main menu.");
+                    continue;
+            }
+
+
+        }
+
     }
+
+    #region  输出功能信息表
+    static void PrintFounciton()
+    {
+        Console.WriteLine("Choose function with Number_input in this Concole:");
+        Console.WriteLine("-----------------------------------");
+        Console.WriteLine("1: Organize Files by CreatedTime.");
+        Console.WriteLine("-----------------------------------");
+        Console.WriteLine("2: LookUp fileVersion Info.");
+        Console.WriteLine("-----------------------------------");
+        Console.WriteLine("3: Go back to MainMenu.");
+        Console.WriteLine("-----------------------------------");
+        Console.WriteLine("|Tips: Input \"exit\" to exit|\r\n");
+    }
+    #endregion
+
+    #region 输出文件Version信息
+    static void OutputFileVersionInfo()
+    {
+        Console.WriteLine("Pls input file Full_Path with Name: ");
+        string userInputPathAndName_console = Console.ReadLine();
+        OrganizeFilesOperation organizeFilesOperation = new OrganizeFilesOperation(userInputPathAndName_console);
+        organizeFilesOperation.PrintFileInfoVersion();
+    }
+    #endregion
+
+    #region  创建文件夹
+    static void CreatDirs()
+    {
+        Console.WriteLine("\r\nPls input organized files DirPath: ");
+        string userInputPath_console = Console.ReadLine();
+        //实例化文件操作对象
+        OrganizeFilesOperation organizeFilesOperation = new OrganizeFilesOperation(userInputPath_console);
+        String[] filesCreatedTimesAll = organizeFilesOperation.FilesCreatedTime();
+        organizeFilesOperation.CreatDirectories(filesCreatedTimesAll);
+        Console.WriteLine("\r\n");
+    }
+    #endregion
+
+    #region EnterToExitConsole
+    static void EnterToExitConsole()
+    {
+        Console.Write("Press <Enter> to exit... \r\n");
+        while (Console.ReadKey().Key != ConsoleKey.Enter) { }
+    }
+    #endregion
+
 }
